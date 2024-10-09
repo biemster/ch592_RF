56d307498bdff123ff7d9301b486c3adab8bf799
Lars Beemster <biemster@users.noreply.github.com>
2024-10-09 11:33:27 +0200

    separate sender and receiver

--- src/RF_PHY.c
+++ src/RF_PHY.c
@@ -11,7 +11,6 @@
  *******************************************************************************/
 
 /******************************************************************************/
-/* 头文件包含 */
 #include "CONFIG.h"
 #include "RF_PHY.h"
 
@@ -19,21 +18,14 @@
  * GLOBAL TYPEDEFS
  */
 
-#define RF_AUTO_MODE_EXAM       1
+// #define RX_MODE
+#define TX_MODE
 
 uint8_t taskID;
-uint8_t TX_DATA[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+uint8_t TX_DATA[] = {1, 2, 3, 42, 3, 2 ,1};
 
-volatile uint8_t tx_end_flag=0;
-volatile uint8_t rx_end_flag=0;
+volatile uint8_t tx_end_flag = 0;
 
-/*********************************************************************
- * @fn      RF_Wait_Tx_End
- *
- * @brief   手动模式等待发送完成，自动模式等待发送-接收完成，必须在RAM中等待，等待时可以执行用户代码，但需要注意执行的代码必须运行在RAM中，否则影响发送
- *
- * @return  none
- */
 __HIGH_CODE
 __attribute__((noinline))
 void RF_Wait_Tx_End()
@@ -52,54 +44,11 @@ void RF_Wait_Tx_End()
     }
 }
 
-/*********************************************************************
- * @fn      RF_Wait_Rx_End
- *
- * @brief   自动模式等待应答发送完成，必须在RAM中等待，等待时可以执行用户代码，但需要注意执行的代码必须运行在RAM中，否则影响发送
- *
- * @return  none
- */
-__HIGH_CODE
-__attribute__((noinline))
-void RF_Wait_Rx_End()
-{
-    uint32_t i=0;
-    while(!rx_end_flag)
-    {
-        i++;
-        __nop();
-        __nop();
-        // 约5ms超时
-        if(i>(FREQ_SYS/1000))
-        {
-            rx_end_flag = TRUE;
-        }
-    }
-}
-
-/*********************************************************************
- * @fn      RF_2G4StatusCallBack
- *
- * @brief   RF 状态回调，此函数在中断中调用。注意：不可在此函数中直接调用RF接收或者发送API，需要使用事件的方式调用
- *          在此回调中直接使用或调用函数涉及到的变量需注意，此函数在中断中调用。
- *
- * @param   sta     - 状态类型
- * @param   crc     - crc校验结果
- * @param   rxBuf   - 数据buf指针
- *
- * @return  none
- */
 void RF_2G4StatusCallBack(uint8_t sta, uint8_t crc, uint8_t *rxBuf)
 {
     switch(sta)
     {
         case TX_MODE_TX_FINISH:
-        {
-#if(!RF_AUTO_MODE_EXAM)
-            tx_end_flag = TRUE;
-#endif
-            break;
-        }
         case TX_MODE_TX_FAIL:
         {
             tx_end_flag = TRUE;
@@ -107,94 +56,48 @@ void RF_2G4StatusCallBack(uint8_t sta, uint8_t crc, uint8_t *rxBuf)
         }
         case TX_MODE_RX_DATA:
         {
-#if(RF_AUTO_MODE_EXAM)
-            tx_end_flag = TRUE;
-            if (crc == 0) {
-                uint8_t i;
-
-                PRINT("tx recv,rssi:%d\n\r", (int8_t)rxBuf[0]);
-                PRINT("len:%d-", rxBuf[1]);
-
-                for (i = 0; i < rxBuf[1]; i++) {
-                    PRINT("%x ", rxBuf[i + 2]);
-                }
-                PRINT("\n\r");
-            } else {
-                if (crc & (1<<0)) {
-                    PRINT("crc error\n\r");
-                }
-
-                if (crc & (1<<1)) {
-                    PRINT("match type error\n\r");
-                }
-            }
-#endif
             break;
         }
         case TX_MODE_RX_TIMEOUT: // Timeout is about 200us
         {
-#if(RF_AUTO_MODE_EXAM)
-            tx_end_flag = TRUE;
-#endif
             break;
         }
         case RX_MODE_RX_DATA:
         {
             if (crc == 0) {
                 uint8_t i;
-#if(RF_AUTO_MODE_EXAM)
-                RF_Wait_Rx_End();
-#endif
-                PRINT("rx recv, rssi: %d\n\r", (int8_t)rxBuf[0]);
+
+                PRINT("rx recv, rssi: %d\r\n", (int8_t)rxBuf[0]);
                 PRINT("len:%d-", rxBuf[1]);
                 
                 for (i = 0; i < rxBuf[1]; i++) {
                     PRINT("%x ", rxBuf[i + 2]);
                 }
-                PRINT("\n\r");
+                PRINT("\r\n");
+                GPIOA_InverseBits(GPIO_Pin_8);
             } else {
                 if (crc & (1<<0)) {
-                    PRINT("crc error\n\r");
+                    PRINT("crc error\r\n");
                 }
 
                 if (crc & (1<<1)) {
-                    PRINT("match type error\n\r");
+                    PRINT("match type error\r\n");
                 }
             }
-#if(!RF_AUTO_MODE_EXAM)
             tmos_set_event(taskID, SBP_RF_RF_RX_EVT);
-#endif
             break;
         }
         case RX_MODE_TX_FINISH:
         {
-#if(RF_AUTO_MODE_EXAM)
-            rx_end_flag = TRUE;
-            tmos_set_event(taskID, SBP_RF_RF_RX_EVT);
-#endif
             break;
         }
         case RX_MODE_TX_FAIL:
         {
-#if(RF_AUTO_MODE_EXAM)
-            rx_end_flag = TRUE;
-            tmos_set_event(taskID, SBP_RF_RF_RX_EVT);
-#endif
             break;
         }
     }
 }
 
-/*********************************************************************
- * @fn      RF_ProcessEvent
- *
- * @brief   RF 事件处理
- *
- * @param   task_id - 任务ID
- * @param   events  - 事件标志
- *
- * @return  未完成事件
- */
 uint16_t RF_ProcessEvent(uint8_t task_id, uint16_t events)
 {
     if(events & SYS_EVENT_MSG)
@@ -218,10 +121,11 @@ uint16_t RF_ProcessEvent(uint8_t task_id, uint16_t events)
     {
         RF_Shut();
         tx_end_flag = FALSE;
-        if(!RF_Tx(TX_DATA, 10, 0xFF, 0xFF))
+        if(!RF_Tx(TX_DATA, sizeof(TX_DATA), 0xFF, 0xFF))
         {
             RF_Wait_Tx_End();
         }
+        GPIOA_InverseBits(GPIO_Pin_8);
         tmos_start_task(taskID, SBP_RF_PERIODIC_EVT, 1000);
         return events ^ SBP_RF_PERIODIC_EVT;
     }
@@ -230,11 +134,8 @@ uint16_t RF_ProcessEvent(uint8_t task_id, uint16_t events)
         uint8_t state;
         RF_Shut();
         TX_DATA[0]++;
-#if(RF_AUTO_MODE_EXAM)
-        rx_end_flag = FALSE;
-#endif
-        state = RF_Rx(TX_DATA, 10, 0xFF, 0xFF);
-        PRINT("RX mode.state = %x\n\r", state);
+        state = RF_Rx(TX_DATA, sizeof(TX_DATA), 0xFF, 0xFF);
+        PRINT("RX mode.state = %x\r\n", state);
         return events ^ SBP_RF_RF_RX_EVT;
     }
     return 0;
@@ -254,31 +155,26 @@ void RF_Init(void)
 
     tmos_memset(&rf_Config, 0, sizeof(rfConfig_t));
     taskID = TMOS_ProcessEventRegister(RF_ProcessEvent);
-    rf_Config.accessAddress = 0x71764129; // 禁止使用0x55555555以及0xAAAAAAAA ( 建议不超过24次位反转，且不超过连续的6个0或1 )
+    rf_Config.accessAddress = 0x71764129;
     rf_Config.CRCInit = 0x555555;
     rf_Config.Channel = 39;
-    rf_Config.Frequency = 2480000;
-#if(RF_AUTO_MODE_EXAM)
-    rf_Config.LLEMode = LLE_MODE_AUTO;
-#else
-    rf_Config.LLEMode = LLE_MODE_BASIC | LLE_MODE_EX_CHANNEL; // 使能 LLE_MODE_EX_CHANNEL 表示 选择 rf_Config.Frequency 作为通信频点
-#endif
+    rf_Config.LLEMode = LLE_MODE_BASIC;
     rf_Config.rfStatusCB = RF_2G4StatusCallBack;
     rf_Config.RxMaxlen = 251;
     state = RF_Config(&rf_Config);
-    PRINT("rf 2.4g init: %x\n\r", state);
+    PRINT("rf 2.4g init: %x\r\n", state);
 
+#ifdef RX_MODE
     { // RX mode
-#if(RF_AUTO_MODE_EXAM)
-        rx_end_flag = FALSE;
-#endif
-        state = RF_Rx(TX_DATA, 10, 0xFF, 0xFF);
-        PRINT("RX mode.state = %x\n\r", state);
+        state = RF_Rx(TX_DATA, sizeof(TX_DATA), 0xFF, 0xFF);
+        PRINT("RX mode.state = %x\r\n", state);
     }
-
-//    { // TX mode
-//        tmos_set_event( taskID , SBP_RF_PERIODIC_EVT );
-//    }
+#endif
+#ifdef TX_MODE
+    { // TX mode
+        tmos_set_event( taskID , SBP_RF_PERIODIC_EVT );
+    }
+#endif
 }
 
 /******************************** endfile @ main ******************************/
